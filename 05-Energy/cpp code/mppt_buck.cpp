450ac8951c654d762a04c7f583cab2523ca4f9d3
Fion Foo <fsf20@ic.ac.uk>
2022-06-07 22:20:06 +0100

    energy data

--- 05-Energy/cpp code/mppt_buck.cpp
+++ 05-Energy/cpp code/mppt_buck.cpp
@@ -5,8 +5,6 @@
 #include <INA219_WE.h>
 
 // A0 - Vb (output voltage)
-// A2 - Vref (potentiometer)
-// A3 - Vpd (part of input voltage)
 
 #define OUTPUT_VOLTAGE A0
 #define RELAY_BATTERY  7 
@@ -35,13 +33,10 @@ float saturation(unsigned int sat_input, unsigned int uplim, unsigned int lowlim
 
 void sampling() {
   
-  sensorValue0 = analogRead(OUTPUT_VOLTAGE); 
-  current_mA = ina219.getCurrent_mA();
+  vb0 = analogRead((OUTPUT_VOLTAGE*4.096/1023)/0.5175); 
+  iL = ina219.getCurrent_mA()/1000;
 
-  analogReference(DEFAULT); // 5V as default for now, external may be more accurate
-  
-  vb0 = sensorValue0*5/1023;
-  iL0 = current_mA/1000.0;
+  analogReference(EXTERNAL);
 
 }
 
@@ -77,26 +72,22 @@ void setup() {
   Wire.setClock(700000);
   ina219.init();
   
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   analogReference(EXTERNAL);
 
   pinMode(13, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
 
   pinMode(6, OUTPUT);
   TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm;
 
-  pwm_modulate(dutyref);
+  pwm_modulate(0.6);
 
   // interrupt for checking output voltage every 1ms
   TCA0.SINGLE.PER = 999; 
   TCA0.SINGLE.CMP1 = 999; 
   TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV16_gc | TCA_SINGLE_ENABLE_bm; //16 prescaler, 1M.
   TCA0.SINGLE.INTCTRL = TCA_SINGLE_CMP1_bm; 
-
-  delay(5000);
 
 }
 
@@ -108,76 +99,72 @@ void loop() {
     sampling();
     calculation(iL0, iL1, vb0, vb1);
 
-    if (vb0 > 5.2){
+    if (vb0 > 5.15 || vb0 < 4.58){
       charger_state = off;
     }
-
-    loopTrigger = false;
-
-  }
 
   switch(charger_state){
 
     case on:
-        if (vb0 < 4.5){
-            pwm_modulate(255);
-            charger_state = off;
-        }
-        else {
-            digitalWrite(7, LOW);
-            charger_state = bulk;
-        }
+      if (vb0 < 4.5){
+          pwm_modulate(255);
+          charger_state = off;
+      }
+      else {
+          digitalWrite(7, HIGH);
+          charger_state = bulk;
+      }
 
     case bulk: // constant current
-        if (dp > 0) {
-            if (dv > 0) {
-                vref += 1;
-            }
-            else {
-                vref -= 1;
-            }
-        }
-        else {
-            if (dv > 0) {
-                vref -= 1;
-            }
-            else {
-                vref += 1;
-            }
-        }
+      if (dp > 0) {
+          if (dv > 0) {
+              vref += 1;
+          }
+          else {
+              vref -= 1;
+          }
+      }
+      else {
+          if (dv > 0) {
+              vref -= 1;
+          }
+          else {
+              vref += 1;
+          }
+      }
 
-        vref = saturation(vref, 5.2, 4.5);
-        pwm_modulate(vref); 
-        
-        vb1 = vb0;
-        iL1 = iL0;
+      vref = saturation(vref, 5.2, 4.5);
+      pwm_modulate(vref); 
+      
+      vb1 = vb0;
+      iL1 = iL0;
 
-        if (vb0 > 5){ // limit for battery voltage
-            charger_state = absorption;
-        }
+      if (vb0 > 5){ // limit for battery voltage
+          charger_state = absorption;
+      }
 
     case absorption: // constant voltage
-        vref = saturation(vref, 5.2, 4.5);
-        ev = vref - vb0;  
-        cv = pidv(ev); 
-        pwm_modulate(cv);
+      vref = saturation(vref, 5.2, 4.5);
+      ev = vref - vb0;  
+      cv = pidv(ev); 
+      pwm_modulate(cv);
 
-        if (iL0 < 0.05){
-            charger_state = flt;
-        }
+      if (iL0 < 0.05){
+          charger_state = flt;
+      }
 
     case flt:
-        vref = 5; // floating voltage to be set
-        ev = vref - vb0;  
-        cv = pidv(ev); 
-        pwm_modulate(cv);
+      vref = 5; // floating voltage to be set
+      ev = vref - vb0;  
+      cv = pidv(ev); 
+      pwm_modulate(cv);
 
-        if (vb0 < 4.5){
-            charger_state = bulk;   
-        }
+      if (vb0 < 4.5){
+          charger_state = bulk;   
+      }
 
     case off:
-        digitalWrite(7, HIGH);
+      digitalWrite(7, LOW);
 
   }
 
@@ -195,8 +182,10 @@ void loop() {
   Serial.print(p0);
   Serial.println("   ");
 
-  digitalWrite(13,LOW);
+  loopTrigger = false;
 
-  delay(1000);
+  }
+
+  digitalWrite(13,LOW);
 
 }
